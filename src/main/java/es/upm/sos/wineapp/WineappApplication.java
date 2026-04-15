5f6e0f64d9609d950ee6857f5fb3d8fe8750ad82
Bayron Viamonte Almonte <bviamontealmonte@gmail.com>
2026-04-15 12:15:18 +0200

    Initial commit

---
+++ src/main/java/es/upm/sos/wineapp/WineappApplication.java
@@ -0,0 +1,13 @@
+package es.upm.sos.wineapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/upm/sos/wineapp/WineappApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.sos.wineapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
